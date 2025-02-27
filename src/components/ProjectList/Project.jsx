32f42059f930c43f64ec5adcb6014944580c9d5d
gabriel-m-m <gabrielmm.00@gmail.com>
2025-02-26 21:37:26 -0800

    Remove set aspect ratio on project thumbnail

--- src/components/ProjectList/Project.jsx
+++ src/components/ProjectList/Project.jsx
@@ -3,7 +3,10 @@ import "./project.css"
 const Project = ({ info }) => {
     return (
         <div className="proj">
-            {info.thumbnail ? <div className="thumbnailcontainer"><img className="thumbnail" src={info.thumbnail}/></div> : null}
+            {info.thumbnail ? 
+                <div className="thumbnailcontainer">
+                    <img className="thumbnail" src={info.thumbnail}/>
+                </div> : null}
             <h3 className="projecttitle">{info.title}</h3>
             <p>{info.description}</p>
             <div className="technologies">
