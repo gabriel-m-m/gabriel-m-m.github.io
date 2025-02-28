4d9f2d8d7227663614c003013b37aefff10dbdeb
gabriel-m-m <gabrielmm.00@gmail.com>
2025-02-27 20:55:12 -0800

    Rewrite project description and add github url

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,22 +2,22 @@ const projects = [
     {
         "thumbnail": "src\\assets\\TripSafe.png",
         "title": "TripSafe",
-        "description": "Created an iOS mobile application using Swift that displays location-based travel safety information. Built an intuitive and readable interface using SwiftUI. Parsed location and directional information by employing the TabularData and MapKit libraries",
+        "description": "A women's travel safety app that offers location-based information for over 190 countries. It includes local emergency phone numbers for fire, ambulance and police services, as well as helplines addressing gender-based violence. It also provides information on levels of peace and women's safety in a user's area.",
         "technologies": ["Swift", "SwiftUI", "MapKit", "TabularData"],
         "github": "https://github.com/gabriel-m-m/TripSafe"
     },
     {
         "thumbnail": "src\\assets\\Ledger.png",
         "title": "Ledger",
-        "description": "Developed a Java application that tracks and records transactions between an arbitrary number of users. Implemented a user-friendly GUI using the Swing library. Utilized automated JUnit testing to ensure proper function of program and quality code",
+        "description": "A ledger application where users can easily record, add, or subtract amounts, keeping a clear record of financial exchanges between one another It includes a clean UI and a detailed test suite. It was built as a term project for a Software Construction course.",
         "technologies": ["Java", "SwingUI", "JUnit"],
         "github": "https://github.com/gabriel-m-m/Ledger-Project"
     },
     {
         "thumbnail": "src\\assets\\DataVisTool.png",
         "title": "Data Visualization Tool",
-        "description": "Developed a program in Python that converts scientific publication data into a graphic based on shared authorship. Employed the Pandas library for data manipulation and the NetworkX and Bokeh libraries to visualize data",
-        "technologies": ["Python", "numpy", "pandas", "NetworkX"],
+        "description": "A python tool that generates an infographic based on shared authorship in scientific publication data. A node represents an author and the thickness of edges between authors increases for higher numbers of co-authored papers.",
+        "technologies": ["Python", "Jupyter", "numpy", "pandas", "NetworkX"],
         "github": "https://github.com/gabriel-m-m/Data-Visualization-Tool"
     }
 ]
