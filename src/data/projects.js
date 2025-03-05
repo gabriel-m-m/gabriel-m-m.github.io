eba4d63554e6fd717fd97dea1410f03d0bf3a15d
gabriel-m-m <gabrielmm.00@gmail.com>
2025-03-04 19:17:05 -0800

    Add border to images and fix CLS issues

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,21 +2,27 @@ const projects = [
     {
         "thumbnail": "src\\assets\\TripSafe.png",
         "title": "TripSafe",
+        "thHeight": 1623,
+        "thWidth": 2055,
         "description": "A women's travel safety app that offers location-based information for over 190 countries. It includes local emergency phone numbers for fire, ambulance and police services, as well as helplines addressing gender-based violence. It also provides information on levels of peace and women's safety in a user's area.",
         "technologies": ["Swift", "SwiftUI", "MapKit", "TabularData"],
         "github": "https://github.com/gabriel-m-m/TripSafe"
     },
     {
         "thumbnail": "src\\assets\\Ledger.png",
+        "thHeight": 798,
+        "thWidth": 1016,
         "title": "Ledger",
-        "description": "A ledger application where users can easily record, add, or subtract amounts, keeping a clear record of financial exchanges between one another It includes a clean UI and a detailed test suite. It was built as a term project for a Software Construction course.",
+        "description": "A ledger application where users can easily record, add, or subtract amounts, keeping a clear record of financial exchanges between one another. It includes a clean UI and a detailed test suite. It was built as a term project for a Software Construction course.",
         "technologies": ["Java", "SwingUI", "JUnit"],
         "github": "https://github.com/gabriel-m-m/Ledger-Project"
     },
     {
         "thumbnail": "src\\assets\\DataVisTool.png",
         "title": "Data Visualization Tool",
-        "description": "A python tool that generates an infographic based on shared authorship in scientific publication data. A node represents an author and the thickness of edges between authors increases for higher numbers of co-authored papers.",
+        "thHeight": 1063,
+        "thWidth": 1425,
+        "description": "A Python tool that generates an infographic based on shared authorship in scientific publication data. A node represents an author and the thickness of edges between authors increases for higher numbers of co-authored papers.",
         "technologies": ["Python", "Jupyter", "numpy", "pandas", "NetworkX"],
         "github": "https://github.com/gabriel-m-m/Data-Visualization-Tool"
     }
