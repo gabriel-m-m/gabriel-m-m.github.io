a90d10d66cd8a86cf316f2d61523a15167e5301b
gabriel-m-m <gabrielmm.00@gmail.com>
2025-03-11 19:53:11 -0700

    Create about me section

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,13 +1,15 @@
 import Skills from "./Skills"
+import AboutMe from "./AboutMe"
 import "./intro.css"
 
 const Intro = () => {
     return (
         <div className="intro">
-            <div>
-                <h3>Hi, my name is</h3>
-                <h1 className="name">Gabriel Moosmann Mendez</h1>
+            <div className="landing">
+                <h3>Hi, I&apos;m Gabriel</h3>
+                <h1 className="name">Software Developer</h1>
             </div>
+            <AboutMe />
             <Skills />
         </div>
     )
--- src/components/Intro/Skills.jsx
+++ src/components/Intro/Skills.jsx
@@ -1,7 +1,17 @@
+import "./skills.css"
+
 const Skills = () => {
+    const technologies = ["TypeScript", "JavaScript", "HTML", "CSS", "React","C#", "Python","C/C++", "SQL", "Java"];
+
     return (
-        <div>
-            <h3>Skills</h3>
+        <div className="skills">
+            <h3>Some of the technolgies I use</h3>
+            <div className="technologies">
+                {technologies.map(tech =>
+                    <div key={tech} className="technology">{tech}</div>
+                )}
+            </div>
+            <span>{"and I'm always eager to learn more :)"}</span>
         </div>
     )
 }
