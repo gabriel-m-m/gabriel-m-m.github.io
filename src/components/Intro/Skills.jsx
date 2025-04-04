e8d546b500e1fc36a72e231df89d91022b7b9d3d
gabriel-m-m <gabrielmm.00@gmail.com>
2025-04-03 20:22:26 -0700

    Improve accessibility

--- src/components/Intro/Skills.jsx
+++ src/components/Intro/Skills.jsx
@@ -5,7 +5,7 @@ const Skills = () => {
 
     return (
         <div className="skills">
-            <h3>Skills</h3>
+            <h2>Skills</h2>
             <div className="technologies">
                 {technologies.map(tech =>
                     <div key={tech} className="technology">{tech}</div>
